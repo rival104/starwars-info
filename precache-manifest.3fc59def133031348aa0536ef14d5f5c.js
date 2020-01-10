ba30f59825f18014edbdc3ba297154eb56151da7
rival104 <navid.rvl@gmail.com>
2020-01-10 00:23:19 -0500

    Updates

---
+++ precache-manifest.3fc59def133031348aa0536ef14d5f5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d8d376fc215c0b3b0f5212693146bd",
+    "url": "/starwar-info/index.html"
+  },
+  {
+    "revision": "54135e910c0d6ff472f5",
+    "url": "/starwar-info/static/css/2.257bd785.chunk.css"
+  },
+  {
+    "revision": "1c846366cc312329e34d",
+    "url": "/starwar-info/static/css/main.ce076ee8.chunk.css"
+  },
+  {
+    "revision": "54135e910c0d6ff472f5",
+    "url": "/starwar-info/static/js/2.558b0f3d.chunk.js"
+  },
+  {
+    "revision": "aacaa6d0203096b46e6921804d6c52f3",
+    "url": "/starwar-info/static/js/2.558b0f3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c846366cc312329e34d",
+    "url": "/starwar-info/static/js/main.c71ad8bf.chunk.js"
+  },
+  {
+    "revision": "b67bf887de6a7372719e",
+    "url": "/starwar-info/static/js/runtime-main.a1a0f41b.js"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/starwar-info/static/media/Starjout.f72f509a.ttf"
+  },
+  {
+    "revision": "6540a7ca64f2e3b5e97fa172678476d1",
+    "url": "/starwar-info/static/media/wookie_sound.6540a7ca.mp3"
+  }
+]);
